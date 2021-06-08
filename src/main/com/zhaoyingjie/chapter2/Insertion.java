3f6968aea3ca77df9c3032e8def843c35deb98bf
YingJie-Zhao <YingJie-Zhao@outlook.com>
2021-06-08 11:04:23 +0800

    Optimize animation

--- src/main/com/zhaoyingjie/chapter2/Insertion.java
+++ src/main/com/zhaoyingjie/chapter2/Insertion.java
@@ -1,8 +1,11 @@
 package com.zhaoyingjie.chapter2;
 
-import java.awt.*;
-
 import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.awt.*;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author zhaoyingjie
@@ -10,29 +13,49 @@ import edu.princeton.cs.algs4.StdDraw;
 @SuppressWarnings({"rawtypes"})
 public class Insertion extends AbstractSort {
 
-    public static void sort(Comparable[] a) throws InterruptedException {
+    public static void sort(Comparable[] a) {
         int n = a.length;
         for (int i = 0; i < n; i++) {
             for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
                 exchange(a, j, j - 1);
                 draw(a, i, j - 1);
-                Thread.sleep(200);
+//                try {
+//                    Thread.sleep(200);
+//                } catch (InterruptedException e) {
+//                    System.out.println(e.getMessage());
+//                }
             }
         }
     }
 
     public static void draw(Comparable[] a, int currentP, int insertionP) {
         StdDraw.clear(Color.BLACK);
-        StdDraw.setPenRadius(0.005);
+        StdDraw.setPenRadius(0.010);
+        StdDraw.enableDoubleBuffering();
         for (int i = 0; i < a.length; i++) {
+            double x0 = 0.01 + i * 0.005;
+            double y0 = 0.1;
+            double x1 = 0.01 + i * 0.005;
+            double y1 = (Double) a[i] / 2 + 0.1;
             if (i == insertionP) {
                 StdDraw.setPenColor(Color.RED);
             } else if (i <= currentP) {
                 StdDraw.setPenColor(Color.cyan);
             } else {
-                StdDraw.setPenColor(Color.LIGHT_GRAY);
+                StdDraw.setPenColor(Color.DARK_GRAY);
             }
-            StdDraw.line(0.1 + i * 0.01, 0.1, 0.1 + i * 0.01, (Double) a[i] / 2 + 0.1);
+            StdDraw.line(x0, y0, x1, y1);
         }
+        StdDraw.show();
+    }
+
+    public static void main(String[] args) {
+        StdDraw.setCanvasSize(2200, 600);
+        List<Double> listA = new LinkedList<>();
+        for (int i = 0; i < 180; i++) {
+            listA.add(Math.abs(StdRandom.uniform()));
+        }
+        Double[] a = listA.toArray(new Double[0]);
+        sort(a);
     }
 }
--- src/main/com/zhaoyingjie/chapter2/Selection.java
+++ src/main/com/zhaoyingjie/chapter2/Selection.java
@@ -1,11 +1,11 @@
 package com.zhaoyingjie.chapter2;
 
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.awt.*;
 import java.util.LinkedList;
 import java.util.List;
-
-import edu.princeton.cs.algs4.StdDraw;
-import edu.princeton.cs.algs4.StdRandom;
 
 /**
  * @author zhaoyingjie
@@ -13,7 +13,7 @@ import edu.princeton.cs.algs4.StdRandom;
 @SuppressWarnings({"rawtypes"})
 public class Selection extends AbstractSort {
 
-    public static void sort(Comparable[] a) throws InterruptedException {
+    public static void sort(Comparable[] a) {
         int n = a.length;
         for (int i = 0; i < n; i++) {
             int min = i;
@@ -24,23 +24,42 @@ public class Selection extends AbstractSort {
             }
             draw(a, i - 1, min);
             exchange(a, i, min);
-            Thread.sleep(200);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 
     public static void draw(Comparable[] a, int currentP, int selectedP) {
         StdDraw.clear(Color.BLACK);
-        StdDraw.setPenRadius(0.005);
-        StdDraw.setYscale();
+        StdDraw.setPenRadius(0.010);
+        StdDraw.enableDoubleBuffering();
         for (int i = 0; i < a.length; i++) {
+            double x0 = 0.01 + i * 0.005;
+            double y0 = 0.1;
+            double x1 = 0.01 + i * 0.005;
+            double y1 = (Double) a[i] / 2 + 0.1;
             if (i == selectedP) {
                 StdDraw.setPenColor(Color.RED);
             } else if (i <= currentP) {
                 StdDraw.setPenColor(Color.cyan);
             } else {
-                StdDraw.setPenColor(Color.LIGHT_GRAY);
+                StdDraw.setPenColor(Color.DARK_GRAY);
             }
-            StdDraw.line(0.1 + i * 0.01, 0.1, 0.1 + i * 0.01, (Double) a[i] / 2 + 0.1);
+            StdDraw.line(x0, y0, x1, y1);
         }
+        StdDraw.show();
+    }
+
+    public static void main(String[] args) {
+        StdDraw.setCanvasSize(2200, 600);
+        List<Double> listA = new LinkedList<>();
+        for (int i = 0; i < 180; i++) {
+            listA.add(Math.abs(StdRandom.uniform()));
+        }
+        Double[] a = listA.toArray(new Double[0]);
+        sort(a);
     }
 }
